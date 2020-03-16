47dec60e78dfe3c0b67090fe75d2d2317ec83925
dlromero <raiga1234@gmail.com>
2020-03-16 14:38:39 -0500

    Decorators python

---
+++ Basic/protected_function.py
@@ -0,0 +1,22 @@
+
+
+def scream(func):
+
+    def wrapper(password):
+        if password == 'platzi':
+            return func()
+        else:
+            print('La contraseña es incorrecta')
+
+    return wrapper
+
+
+@scream
+def protected_func():
+    print('Tu contraseña es correcta.')
+
+
+if __name__ == '__main__':
+    password = str(input('Ingresa tu contraseña: '))
+
+    protected_func(password)
